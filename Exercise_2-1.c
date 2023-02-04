093910ecb508b1c7e691ba9b3a792b7f13ef2e76
Parvez004 <riadprvzz@gmail.com>
2023-02-04 11:28:12 +0600

    matha-kharap

---
+++ Exercise_2-1.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    int a, b, c;
+    printf("Enter the value of a, b and c:\n");
+    scanf("%d%d%d", &a, &b, &c);
+    printf("Ecuation is: %dx + %dy = %d", a, b, c);
+    return 0;
+}
---
+++ Exercise_2-3.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int i, x;
+    for (i = 1; i <= 10; i++)
+    {
+        x=5*i;
+        printf("5 X %d = %d\n", i, x);
+    }
+    return 0;
+}
